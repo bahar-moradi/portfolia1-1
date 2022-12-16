269b758a22d9671e2904230d292393a592f79726
bahar-moradi <bahar.moradi33@gmail.com>
2022-12-16 11:44:11 +0100

    create component and make initial change

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about1/about1'
+import Experience from './components/experience/experience'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/about1/about1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about1.css'
+
+const about1 = () => {
+  return (
+    <div>about1</div>
+  )
+}
+
+export default about1
